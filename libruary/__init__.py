44364f8b037a7b456f117e6002a3ab99a6654062
gea <egor.grinevich@gmail.com>
2023-05-14 20:33:54 +0300

    Last merge with libruary

---
+++ libruary/__init__.py
@@ -0,0 +1,5 @@
+from libruary.np_array import (
+    odd_array,
+    odd_array_index,
+    sort_array
+    )
--- main.py
+++ main.py
@@ -1,39 +1,20 @@
 import numpy as np
 import csv
-
-def odd_array(ar):
-    odd_ar = np.array([], 'int')
-    for i in range(len(ar)):
-        if (ar[i] % 2 != 0):
-            odd_ar = np.append(odd_ar, ar[i], axis=None)
-    return odd_ar
-
-def odd_array_index(ar):
-    odd_ar = np.array([], 'int')
-    for i in range(1, len(ar), 2):
-        odd_ar = np.append(odd_ar, ar[i], axis=None)
-    return odd_ar
-
-
+from libruary import np_array
 
 rand_array = np.random.randint(0, 100, 100)
 print(rand_array)
 
-for i in range(len(rand_array)-1):
-    for j in range(len(rand_array)-i-1):
-        if rand_array[j] > rand_array[j+1]:
-            m = rand_array[j]
-            rand_array[j] = rand_array[j+1]
-            rand_array[j+1] = m
+rand_array = np_array.sort_array(rand_array)
 
 
-np_file = open('np_array.csv', 'w')
+np_file = open('output_file/np_array.csv', 'w')
 with np_file:
     writer = csv.writer(np_file)
     writer.writerow(rand_array)
-    writer.writerow(odd_array(rand_array))
-    writer.writerow(odd_array_index(rand_array))
+    writer.writerow(np_array.odd_array(rand_array))
+    writer.writerow(np_array.odd_array_index(rand_array))
 
 print(rand_array)
-print(odd_array(rand_array))
-print(odd_array_index(rand_array))
+print(np_array.odd_array(rand_array))
+print(np_array.odd_array_index(rand_array))
