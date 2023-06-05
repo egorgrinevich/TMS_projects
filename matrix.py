170a4edff62bda598c3e0d5a761370c2a0ff9008
gea <egor.grinevich@gmail.com>
2023-06-05 17:54:29 +0300

    multiply matrix

--- matrix.py
+++ matrix.py
@@ -6,22 +6,24 @@ a = [[1, 1, 0],
 b = [[1, 0, 2, 1],
      [2, 1, 2, 0],
      [1, 1, 0, 3]]
-def vec_product(vec1: List[int], vec2: List[int]) -> int:
+def mult_vec(vec1: List[int], vec2: List[int]) -> int:
     return sum([int(x * y) for x, y in zip(vec1, vec2)])
 
 
-def matrix_transpose(mat: List[List]) -> List[List]:
+def tran_matr(mat: List[List]) -> List[List]:
     return [*map(list, zip(*mat))]
 
 
-def matrix_product(mat1: List[List[int]], mat2: List[List[int]]):
+def mult_matr(mat1: List[List[int]], mat2: List[List[int]]):
     l, n = len(mat1), len(mat2[0])
-    ans = [[0 for i in range(n)] for j in range(l)]
-    for i in range(l):
-        for j in range(n):
-            vec1 = mat1[i]
-            vec2 = matrix_transpose(mat2)[j]
-            ans[i][j] = vec_product(vec1, vec2)
-    return ans
+    if len(mat1[0])==len(mat2):
+        ans = [[0 for i in range(n)] for j in range(l)]
+        for i in range(l):
+            for j in range(n):
+               vec1 = mat1[i]
+               vec2 = tran_matr(mat2)[j]
+               ans[i][j] = mult_vec(vec1, vec2)
+        return ans
+    else: return "матрицы не совместимы"
 
-print(matrix_product(a,b))
+print(mult_matr(a,b))
